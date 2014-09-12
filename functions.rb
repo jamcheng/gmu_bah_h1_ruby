9eeae4f3f9378b424ee9d7be883759b3bb4d4bff
Jamie Cheng <jacheng@vt.edu>
2014-09-11 21:35:57 -0400

    adding functions.rb

---
+++ functions.rb
@@ -0,0 +1,12 @@
+# return reversed copy of the array
+def reverse(an array)
+	an array.reverse
+end
+
+# Return a map of letters and counts of letters
+# Letters should be lowerscase before counting
+# For example, histogram ('Hello') returns {'h'=>1,'e'=>1,'l'=>2, 'o'>=1 }
+#def histogram(a string)
+	#code here
+#end
+puts reverse([3,6,'dog']).inspect
