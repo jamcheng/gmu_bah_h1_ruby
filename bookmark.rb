5519f9d492251a938ba66524318c79535c06e0eb
Jamie Cheng <jacheng@vt.edu>
2014-09-18 00:30:40 -0400

    bookmark.rb draft

---
+++ bookmark.rb
@@ -0,0 +1,15 @@
+class Bookmark
+	def bookmark
+		attr_accessor :url, :title
+		attr_reader :last_visited
+	end
+	def intialize(url, title, last_visited)
+		@url = url.new
+		@title = title.new
+		@last_visited=time.new
+	end
+	def visit!(last_visited)
+		last_visited=last_visited.time.now
+	end
+end
+puts intialize('https://google.com','search window').inspect
